fdab6415b77278e7b1eb666952b712657738dd18
Congencio Juliao <wagnerlthe7@email.com>
2025-10-08 09:08:25 +0200

    debug: simplify revoke_others_handler to minimum

--- server/apps/chat_app/src/revoke_others_handler.erl
+++ server/apps/chat_app/src/revoke_others_handler.erl
@@ -1,24 +1,6 @@
-%%%-------------------------------------------------------------------
-%%% revoke_others_handler.erl
-%%% Handler para /auth/revoke-others (revoga todas as outras sessões)
-%%%-------------------------------------------------------------------
 -module(revoke_others_handler).
--behaviour(cowboy_handler).
-
 -export([init/2]).
 
--include_lib("kernel/include/logger.hrl").
-
 init(Req0, State) ->
-    Method = cowboy_req:method(Req0),
-    case Method of
-        <<"POST">> ->
-            {ok, Body, Req1} = cowboy_req:read_body(Req0),
-            ?LOG_INFO("🚫 /auth/revoke-others Body: ~p", [Body]),
-            %% Resposta simples sem usar firebase_handler:reply_json
-            ReqF = cowboy_req:reply(200, #{<<"content-type">> => <<"application/json">>}, <<"{\"ok\":\"other_sessions_revoked\"}">>, Req1),
-            {ok, ReqF, State};
-        _ ->
-            ReqF = cowboy_req:reply(405, #{<<"content-type">> => <<"application/json">>}, <<"{\"error\":\"method_not_allowed\"}">>, Req0),
-            {ok, ReqF, State}
-    end.
+    ReqF = cowboy_req:reply(200, #{<<"content-type">> => <<"application/json">>}, <<"{\"ok\":\"test\"}">>, Req0),
+    {ok, ReqF, State}.
